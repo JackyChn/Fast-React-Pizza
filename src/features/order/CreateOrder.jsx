000b0595ac9684ef018992e46f4d872ea63c1a09
JackyChn <ccc1020048076@gmail.com>
2024-09-03 19:11:40 +0930

    Add: error handling in actions and use useActionData() hook to show error

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,6 @@
 // https://uibakery.io/regex-library/phone-number
 import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 const isValidPhone = (str) =>
@@ -34,7 +34,10 @@ const fakeCart = [
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  // const cart = fakeCart;
+  const formErrors = useActionData();
 
   return (
     <div>
@@ -54,6 +57,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
         {/* address input box */}
         <div>
@@ -78,7 +82,9 @@ function CreateOrder() {
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
         </div>
         <div>
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Placing order..." : "Place order"}
+          </button>
         </div>
       </Form>
     </div>
@@ -93,7 +99,12 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priorty === "on",
   };
-  console.log(order);
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone = "Please enter the correct phone number.";
+  if (Object.keys(errors).length > 0) return errors;
+
+  // no error, create new order & redirect
   const newOrder = await createOrder(order);
   return redirect(`/order/${newOrder.id}`);
 }
