c15c2fa10141c929be559dca6d9d001a4550ebfa
JackyChn <ccc1020048076@gmail.com>
2024-09-05 00:51:16 +0930

    Add: Customize color, fontFamilyy, fontSize, etc in the tailwind.config.css

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import UserName from "../features/user/UserName";
 
 function Header() {
   return (
-    <header className="border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+    <header className="font-pizza border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizz Co.
       </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+    extend: {
+      colors: {
+        // keep the origin color and extend new colors, must put in the colors
+        pizza: "#12345",
+      },
+      fontSize: {
+        // same as color, put customized fontSize here
+        huge: ["10rem", { lineHeight: "1" }],
+      },
+      height: {
+        screen: "100dvh", // dynamic
+      },
+    },
   },
   plugins: [],
 };
